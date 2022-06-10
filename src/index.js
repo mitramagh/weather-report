2510124f869131b3677709c4c9f86ef97a913a05
Lulu Sun <sunrey32@uw.edu>
2022-06-09 20:13:45 -0700

    finished wave 3 and 5

--- src/index.js
+++ src/index.js
@@ -1,49 +1,84 @@
-"use strict";
+'use strict';
 
+// let addBtn = document.querySelector('#add');
+// let subBtn = document.querySelector('#sub');
+// let qty = document.querySelector('#qtyBox');
 
+// addBtn.addEventListener('click',()=>{
+//   qty.value = parseInt(qty.value) + 1;
+// });
 
+// subBtn.addEventListener('click',()=>{
+//   if (qty.value <= 0) {
+//     qty.value = 0;
+//   }
+//   else{
+//     qty.value = parseInt(qty.value) - 1;
+//   }
+// });
 
-let addBtn = document.querySelector('#add');
-let subBtn = document.querySelector('#sub');
-let qty = document.querySelector('#qtyBox');
+// const getCurrentTemp = () => {
+//   const currentTemp = new Temp??();
+//   return currentTemp;
+// }
 
-addBtn.addEventListener('click',()=>{
-  qty.value = parseInt(qty.value) + 1;
-});
+// const increaseTemperature = () => {
+//   const newTemperature = temperatureValue + 1;
+//   setTemperatureValue(newTemperature);
+//   if (newTemperature >= 76) {
+//     setTemperatureColor('hot');
+//   }
+// };
 
-subBtn.addEventListener('click',()=>{
-  if (qty.value <= 0) {
-    qty.value = 0;
+// const decreaseTemperature = () => {
+//   const newTemperature = temperatureValue - 1;
+//   setTemperatureValue(newTemperature);
+//   if (newTemperature < 65) {
+//     setTemperatureColor('cold');
+//   }
+// };
+
+// const temp = 70; // use API call to get the real temp
+// const updateTemp = () => {
+//     document.getElementById("TempPlaceholder").innerHTML = temp;
+// }
+
+const cityID = document.getElementById('cityid');
+const dispalyName = document.getElementById('display-name');
+const displaySky = document.getElementById('display-sky');
+const skyType = document.getElementById('skytype');
+
+const updateCity = () => {
+  dispalyName.textContent = cityID.value;
+};
+
+const resetCity = () => {
+  cityID.value = '';
+  state.city = 'Seattle';
+  dispalyName.textContent = state.city;
+  getCurrentTemp();
+};
+
+const changeSky = (e) => {
+  const skyType = e.target.value;
+  if (skyType == 'cloudy') {
+    displaySky.textContent = '☁️';
   }
-  else{
-    qty.value = parseInt(qty.value) - 1;
+  if (skyType == 'rainy') {
+    displaySky.textContent = '☔️';
   }
-});
-
-
-const getCurrentTemp = () => {
-  const currentTemp = new Temp??();
-  return currentTemp;
-}
-
-
-const increaseTemperature = () => {
-  const newTemperature = temperatureValue + 1;
-  setTemperatureValue(newTemperature);
-  if (newTemperature >= 76) {
-    setTemperatureColor('hot');
+  if (skyType == 'snowy') {
+    displaySky.textContent = '❄️';
+  }
+  if (skyType == 'sunny') {
+    displaySky.textContent = '☀️';
   }
 };
 
-const decreaseTemperature = () => {
-  const newTemperature = temperatureValue - 1;
-  setTemperatureValue(newTemperature);
-  if (newTemperature < 65) {
-    setTemperatureColor('cold');
-  }
+const registerEventHandlers = () => {
+  cityID.addEventListener('input', updateCity);
+
+  skyType.addEventListener('change', changeSky);
 };
 
-var temp = 70; // use API call to get the real temp
-            function updateTemp() {                
-                document.getElementById("TempPlaceholder").innerHTML = temp;
-            }
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
