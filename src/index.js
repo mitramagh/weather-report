1c03e4e5f7c5e6462b4fd8d786514e0445a6921c
mit <mit.magh@gmail.com>
2022-06-10 12:06:45 -0700

    added API for location and weather

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const cityID = document.getElementById('cityid');
-const dispalyName = document.getElementById('display-name');
+const displayName = document.getElementById('display-name');
 const displaySky = document.getElementById('display-sky');
 const skyType = document.getElementById('skytype');
 const reset = document.getElementById('reset');
@@ -9,7 +9,7 @@ const defaultCity = 'Seattle';
 
 const state = {
   city: 'Seattle',
-  temperature: 65,
+  temperature: 0
 };
 
 const addBtn = () => {
@@ -47,16 +47,55 @@ const changeColorAndLandscapeBasedOnTemp = () => {
 }
 };
 
+let lat = 0;
+let lon = 0;
+const getLocation = () => {
+  axios.get('http://localhost:5000/location', {params:{ 'q':displayName.textContent}})
+  .then((response) => {
+      console.log(response);
+      lat =response.data[0].lat;
+      lon =response.data[0].lon;
+      
+      console.log("Location: ", displayName.textContent);
+      // getWeather();
+      console.log(lat, lon);
+  }).catch((error) => {
+      console.log(error);
+  })
+}
+
+
+const requsetWeatherData = () => {
+  axios.get('http://localhost:5000/weather', {params:{'lat':lat , 'lon':lon }})
+      .then((response) => {
+          console.log(response);
+          const kelvin= response.data.current.temp;
+          // (0K − 273.15) × 9/5 + 32 = -459.7°F
+          state.temperature = (kelvin - 273.15) * 9/5 + 32;
+      }).catch((error) => {
+          console.log(error);
+        })
+  }
+      
+      
+//       req .then((response) => {
+//           console.log(response.data.lat, response.data.lon)
+//       })
+//       .catch ((error) => {
+//           console.log(error.response.statusText)
+//       })
+  
+
 
 
 
 const updateCity = () => {
-  dispalyName.textContent = cityID.value;
+  displayName.textContent = cityID.value;
 };
 
 const resetCity = () => {
   cityID.value = '';
-  dispalyName.textContent = defaultCity;
+  displayName.textContent = defaultCity;
 };
 
 const changeSky = (e) => {
@@ -93,6 +132,7 @@ const registerEventHandlers = () => {
   skyType.addEventListener('change', changeSky);
 
   reset.addEventListener('click', resetCity);
+  getLocation();
+  requsetWeatherData();
 };
-
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
